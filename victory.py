49eaff37c0e517ed33b4f5f84d6cc4746d2ec9e6
Banchev1985 <55958209+Banchev1985@users.noreply.github.com>
2019-10-15 22:43:17 +0300

    Add files via upload

--- victory.py
+++ victory.py
@@ -42,3 +42,44 @@ print('У вас', correct, 'правильных ответов.')
 print('У вас', incorrect, 'неправильных ответов.')
 print('У Вас', vol, '% правильных ответов.')
 print('У Вас', vol_2, '% неправильных ответов.')
+restsrt = input('Вы хотите начать заново?' 'Да/Нет ')
+while restsrt == 'Да':
+    correct = 0
+    incorrect = 0
+
+    by_1 = input('В каком году родился  Юрий Гагарин? ')
+    if by_1 == '1934':
+        correct += 1
+    else:
+        incorrect += 1
+
+    by_2 = input('В каком году родился  Дмитрий Нагиев? ')
+    if by_2 == '1967':
+        correct += 1
+    else:
+        incorrect += 1
+
+    by_3 = input('В каком году родился  Владимир Путин? ')
+    if by_3 == '1952':
+        correct += 1
+    else:
+        incorrect += 1
+
+    by_4 = input('В каком году родился  Никола Тесла? ')
+    if by_4 == '1856':
+        correct += 1
+    else:
+        incorrect += 1
+
+    by_5 = input('В каком году родился  Альберт Эйнштейн? ')
+    if by_5 == '1879':
+        correct += 1
+    else:
+        incorrect += 1
+    vol = correct / 5 * 100
+    vol_2 = incorrect / 5 * 100
+    print('У вас', correct, 'правильных ответов.')
+    print('У вас', incorrect, 'неправильных ответов.')
+    print('У Вас', vol, '% правильных ответов.')
+    print('У Вас', vol_2, '% неправильных ответов.')
+    restsrt = input('Вы хотите начать заново?' 'Да/Нет ')
